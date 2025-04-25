c89f95fdae620bcff26f214c2e94ee95388b61b9
YaghitoYT <yago.ninu31@gmail.com>
2025-04-25 19:44:27 +0200

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const fs = require('fs');
 const path = require('path');
 
 let mainWindow;
@@ -16,6 +17,27 @@ function createWindow() {
 
   mainWindow.loadFile('index.html');
 }
+
+// Handle file open
+ipcMain.handle('dialog:openFile', async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'All Files', extensions: ['*'] },
+      { name: 'JavaScript', extensions: ['js'] },
+      { name: 'HTML', extensions: ['html'] },
+      { name: 'CSS', extensions: ['css'] },
+      { name: 'Text Files', extensions: ['txt'] }
+    ]
+  });
+
+  if (canceled) {
+    return null;
+  } else {
+    const fileContent = fs.readFileSync(filePaths[0], 'utf-8');
+    return { filePath: filePaths[0], content: fileContent };
+  }
+});
 
 app.whenReady().then(createWindow);
 
